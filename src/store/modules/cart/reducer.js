7581b0f93a4e1bc1ad0b69fa43b657a1b84071e7
MiguelBragaGarcia <miguelbragagarcia@gmail.com>
2020-02-28 13:15:28 -0300

    RocketShoes Web-Market

---
+++ src/store/modules/cart/reducer.js
@@ -0,0 +1,40 @@
+import produce from 'immer'; // immer é uma biblioteca ponte entre a mutabilidade e imutabilidade dentro de códigos imutaveis
+
+export default function cart(state = [], action) {
+    switch (action.type) {
+        case '@cart/ADD':
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(
+                    p => p.id === action.product.id
+                );
+                if (productIndex >= 0) {
+                    draft[productIndex].amount += 1;
+                } else {
+                    draft.push({
+                        ...action.product,
+                        amount: 1,
+                    });
+                }
+            });
+        case '@cart/REMOVE':
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(p => p.id === action.id);
+                if (productIndex >= 0) {
+                    draft.splice(productIndex, 1); // Remove 1 item a partir do index retornado
+                }
+            });
+        case '@cart/UPDATE_AMOUNT': {
+            if (action.amount <= 0) {
+                return state;
+            }
+            return produce(state, draft => {
+                const productIndex = draft.findIndex(p => p.id === action.id);
+                if (productIndex >= 0) {
+                    draft[productIndex].amount = Number(action.amount); // Remove 1 item a partir do index retornado
+                }
+            });
+        }
+        default:
+            return state;
+    }
+}
